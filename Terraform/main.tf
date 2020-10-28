0a342a09c707449148481f715d6863b5aa863ee3
chaitu.bobby66@gmail.com <chaitu.bobby66@gmail.com>
2020-10-28 23:39:05 +0530

    updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,76 +1,37 @@
-provider "azurerm" {
-}
+terraform {
+   required_version = ">= 0.11" 
+  backend "azurerm" {
+   storage_account_name = "__terraformstorageaccount__"
+     container_name       = "terraform"
+     key                  = "terraform.tfstate"
+ 	access_key  ="__storagekey__"
+   features{}
+ 	}
+ 	}
+   provider "azurerm" {
+     version = "=2.0.0"
+ features {}
+ }
+ resource "azurerm_resource_group" "dev" {
+   name     = "PULTerraform"
+   location = "West Europe"
+ }
 
-variable "prefix" {
-  default = "ITinfraTutorial"
-}
+ resource "azurerm_app_service_plan" "dev" {
+   name                = "__appserviceplan__"
+   location            = "${azurerm_resource_group.dev.location}"
+   resource_group_name = "${azurerm_resource_group.dev.name}"
 
-resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-resources"
-  location = "West US 2"
-}
+   sku {
+     tier = "Free"
+     size = "F1"
+   }
+ }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
-}
+ resource "azurerm_app_service" "dev" {
+   name                = "__appservicename__"
+   location            = "${azurerm_resource_group.dev.location}"
+   resource_group_name = "${azurerm_resource_group.dev.name}"
+   app_service_plan_id = "${azurerm_app_service_plan.dev.id}"
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = "${azurerm_resource_group.main.name}"
-  virtual_network_name = "${azurerm_virtual_network.main.name}"
-  address_prefix       = "10.0.2.0/24"
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.internal.id}"
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = "${azurerm_resource_group.main.location}"
-  resource_group_name   = "${azurerm_resource_group.main.name}"
-  network_interface_ids = ["${azurerm_network_interface.main.id}"]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
-}
+ }
